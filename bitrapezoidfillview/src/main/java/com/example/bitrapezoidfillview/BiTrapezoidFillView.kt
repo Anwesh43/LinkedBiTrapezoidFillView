1902e674e4d2056bf56db6bdb3eb74349db86d4e
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 00:43:29 +0530

    adding constants and libraries

--- bitrapezoidfillview/src/main/java/com/example/bitrapezoidfillview/BiTrapezoidFillView.kt
+++ bitrapezoidfillview/src/main/java/com/example/bitrapezoidfillview/BiTrapezoidFillView.kt
@@ -1,2 +1,28 @@
 package com.example.bitrapezoidfillview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#4CAF50",
+    "#673AB7",
+    "#FFC107",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 7
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
