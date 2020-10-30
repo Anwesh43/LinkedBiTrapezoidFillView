8e0ed3a5c3093882100e0bd9190f07034a4fc465
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 01:42:47 +0530

    adding Renderer class

--- bitrapezoidfillview/src/main/java/com/example/bitrapezoidfillview/BiTrapezoidFillView.kt
+++ bitrapezoidfillview/src/main/java/com/example/bitrapezoidfillview/BiTrapezoidFillView.kt
@@ -193,4 +193,27 @@ class BiTrapezoidFillView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiTrapezoidFillView) {
+
+        private val animator : Animator = Animator(view)
+        private val btf : BiTrapezoidFill = BiTrapezoidFill(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            btf.draw(canvas, paint)
+            animator.animate {
+                btf.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            btf.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
