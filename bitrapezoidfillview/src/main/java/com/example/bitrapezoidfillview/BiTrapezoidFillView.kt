8cb326ee8109ac5bed297fe081f0d7b03d89ab93
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 01:12:48 +0530

    adding BiTrapezoidFillView class

--- bitrapezoidfillview/src/main/java/com/example/bitrapezoidfillview/BiTrapezoidFillView.kt
+++ bitrapezoidfillview/src/main/java/com/example/bitrapezoidfillview/BiTrapezoidFillView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawBTFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiTrapezoidFill(scale, w, h, paint)
 }
+
+class BiTrapezoidFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
